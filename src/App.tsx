47770109e1ef2390a4aaf35311facc8e282d1783
andreseichi <andre_seichi@hotmail.com>
2022-11-24 12:03:46 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { FormEvent, useState } from "react";
+import { SearchResults } from "./components/SearchResults";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
+  return (
+    <div className="App">
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+  const data = {
+    products: [],
+  };
+
+  for (let i = 0; i < 1000; i++) {
+    data.products.push({
+      id: i + 1,
+      price: 199,
+      title: "Product " + (i + 1),
+    });
+  }
+
+  return data;
+};
